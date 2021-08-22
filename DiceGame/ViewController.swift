889bb093bad913af3a788f6135d687f673356c29
alican <alicanyilmaz101@gmail.com>
2021-08-22 17:02:18 +0300

    Initial Commit

---
+++ DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by alican on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
