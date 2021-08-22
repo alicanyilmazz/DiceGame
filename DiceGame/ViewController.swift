d7f4b6c9e31e07c4202b07fe442ad77c953c6041
alican <alicanyilmaz101@gmail.com>
2021-08-22 22:50:40 +0300

    Objects defined.

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblGamerOneScore: UILabel!
+    
+    @IBOutlet weak var lblGamerTwoScore: UILabel!
+    
+    @IBOutlet weak var lblGamerOnePuan: UILabel!
+    
+    @IBOutlet weak var lblGamerTwoPuan: UILabel!
+    
+    @IBOutlet weak var ImgGamerOneState: UIImageView!
+    
+    @IBOutlet weak var ImgGamerTwoState: UIImageView!
+    
+    @IBOutlet weak var ImgDiceOne: UIImageView!
+    
+    @IBOutlet weak var ImgDiceTwo: UIImageView!
+    
+    @IBOutlet weak var lblSetResult: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
